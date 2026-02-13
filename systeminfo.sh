4da213d363882d024ed98e80fb625f4f28e91d9b
Bhuvan Sreedharam <200482627+Myname134@users.noreply.github.com>
2026-02-13 10:12:24 +0530

    Enhance system info script: colors, selective options, formatted memory

--- systeminfo.sh
+++ systeminfo.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo "======"
-echo "System information"
-echo "       ==========="
+RED="\e[31m"
+GREEN="\e[32m"
+YEL="\e[33m"
+BLUE="\e[34m"
+MAG="\e[35m"
+WH="\e[37m"
+RESET="\e[0m"
+
+echo "=================="
+echo -e "${GREEN}System information${RESET}"
+echo "=================="
 
 system_info() {
   echo ""
-  echo "S=Y=S=T=E=M"
+  echo -e "${WH}SYSTEM${RESET}"
   echo "Hostname     : $(hostname)"
   echo "OS           : $(uname -o)"
   echo "Kernel       : $(uname -r)"
@@ -16,34 +24,59 @@ system_info() {
 
 cpu_info() {
   echo ""
-  echo "C-P-U"
+  echo -e "${MAG}CPU${RESET}"
   echo "Model  : $(grep 'model name' /proc/cpuinfo | head -n 1 | cut -d ':' -f2)"
   echo "Cores : $(grep -c ^processor /proc/cpuinfo)"
 }
 
 memory_info() {
   echo ""
-  echo "M%E%M%O%R%Y"
-  free -h
+  echo -e "${BLUE}MEMORY${RESET}"
+  free -h | awk '/Mem:/ {print "Total: "$2" | Used: "$3" | Free: "$4}'
 }
 
 disk_info() {
   echo ""
-  echo "D^I^S^K^U^S^A^G^E"
+  echo -e "${RED}DISKUSAGE${RESET}"
   df -h
 }
 
 network_info() {
   echo ""
-  echo "N#E#T#W#O#R#K"
-  ip addr show | grep -E 'inet ' | awk '{print $2}'
+  echo -e "${YEL}NETWORK${RESET}"
+  if command -v ip &>/dev/null; then
+    ip addr show | grep -E 'inet ' | awk '{print $2}'
+  else
+    echo "ip command not found"
+  fi
 }
 
-system_info
-cpu_info
-memory_info
-disk_info
-network_info
+show_all=false
+
+while getopts "scmdna" opt; do
+  case $opt in
+  s) system_info ;;
+  c) cpu_info ;;
+  m) memory_info ;;
+  d) disk_info ;;
+  n) network_info ;;
+  a) show_all=true ;;
+  *)
+    echo "Usage: $0 [-s] [-c] [-m] [-d] [-n] [-a]"
+    exit 1
+    ;;
+  esac
+done
+
+if [$OPTIND -eq 1] || [ "$show_all" = true ]; then
+  system_info
+  cpu_info
+  memory_info
+  disk_info
+  network_info
+fi
 
 echo ""
-echo "*((*(*((*(*(*((*((*(*(*(*(*((*(*"
+echo "======="
+echo -e "${GREEN}THE END${RESET}"
+echo "======="
